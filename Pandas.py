2136740ba419291bcdb8ed623bfaa4f1983e0f3c
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-19 15:00:28 -0300

    Sintaxes da biblioteca de pandas.

--- Pandas.py
+++ Pandas.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 dados_ibge = pd.read_csv('dados.csv')
-print(dados_ibge.head())
-## print(dados_ibge) # aqui o código, "pd.read_csv", uma sintaxe do pandas que permite ler arquivos DATAFRAME (JSON, CSV e etc)
+print(dados_ibge.head()) # aqui, .head() irá exibir as 5 primeiras linhas de dados do arquivo.csv
+
+print(dados_ibge) # aqui o código, "pd.read_csv", uma sintaxe do pandas que permite ler arquivos DATAFRAME (JSON, CSV e etc)
+
+# ---------
+# Separando os dados por coluna:
+
+print(sorted(dados_ibge['UF'].unique().tolist())) # aqui, separei para visualizar apenas a coluna 'UF', em ordem do menor pro maior.
