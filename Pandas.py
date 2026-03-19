e999ff77e098238f22c26c9be29a952438552ea0
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-19 15:29:25 -0300

    Tipos de Variaveis Quantitativas e Qualitativas

--- Pandas.py
+++ Pandas.py
@@ -9,3 +9,11 @@ print(dados_ibge) # aqui o código, "pd.read_csv", uma sintaxe do pandas que per
 # Separando os dados por coluna:
 
 print(sorted(dados_ibge['UF'].unique().tolist())) # aqui, separei para visualizar apenas a coluna 'UF', em ordem do menor pro maior.
+print(sorted(dados_ibge['Anos de Estudo'].unique().tolist()))
+print(sorted(dados_ibge['Sexo'].unique().tolist()))
+print(sorted(dados_ibge['Cor'].unique().tolist()))
+
+# ----------
+# Vizualizando qual é o menor dado e o maior dado de determinada coluna ( idade )
+print('A menor idade é: ', dados_ibge.Idade.min())
+print('A menor idade é: ', dados_ibge.Idade.max())
