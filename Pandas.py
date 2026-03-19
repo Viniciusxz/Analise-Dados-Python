ef6f09d3b09d08a9d57ff05dd2ec2ce4d401b5a7
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-19 14:48:39 -0300

    Sintaxe de pandas para mostrar apenas os 5 primeiros elementos da lista de dados.

--- Pandas.py
+++ Pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 dados_ibge = pd.read_csv('dados.csv')
-print(dados_ibge)
+print(dados_ibge.head())
+## print(dados_ibge) # aqui o código, "pd.read_csv", uma sintaxe do pandas que permite ler arquivos DATAFRAME (JSON, CSV e etc)
