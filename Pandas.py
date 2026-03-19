a8b7fb3dfb081da23274fe81e7c0c2e10be5e657
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-19 15:32:22 -0300

    Contagem com e sem porcentagem para determinada coluna de dados

--- Pandas.py
+++ Pandas.py
@@ -17,3 +17,17 @@ print(sorted(dados_ibge['Cor'].unique().tolist()))
 # Vizualizando qual é o menor dado e o maior dado de determinada coluna ( idade )
 print('A menor idade é: ', dados_ibge.Idade.min())
 print('A menor idade é: ', dados_ibge.Idade.max())
+
+# ----- 
+# Tipos de Variaveis em analise de dados:
+# Variavel Qualitativa Nominal = Não existe Hierarquia
+# -> Exemplo: Cor, Estado, Gênero.
+# Variavel qualitativa Ordinal = Tem uma ordem lógica (Hierarquica)
+# -> Exemplo: Escolaridade Fundamental < Médio < Superior
+# Variavel Quantitativa Discretas = Números inteiros (contagem)
+# - Exemplo: Filhos = 2
+# Variavel Quantitativa Contínuas = Números decimais
+# - Exemplo: Altura = 1.75
+
+print(dados_ibge['Sexo'].value_counts()) # Aqui, uma sintaxe que irá pegar a coluna 'sexo' e contar quantos registros tem de feminino e masculino (1 e 0)
+print(dados_ibge['Sexo'].value_counts(normalize = True)) # A mesma situação que a sintaxe acima, mas nesse, o normalize irá criar uma diferença de porcentagem entre cada genero
